a61c3a3791326cc448f33c49a841ea34276f9992
Nakibul Shaki <nakibulshaki@hotmail.com>
2024-01-20 17:28:18 -0500

    Add Global Exception Middleware

---
+++ QdaoCaseManager/QdaoCaseManager/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+
+namespace QdaoCaseManager.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, $"Something went wrong: {ex}");
+            }
+        }
+
+    }
+}
