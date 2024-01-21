141d05c26b7da91ce3e32a390a4b4a4e377ab21e
Nakibul Shaki <nakibulshaki@hotmail.com>
2024-01-21 18:58:48 -0500

    Move Entites

---
+++ QdaoCaseManager/QdaoCaseManager/Entites/AuditRoot.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace QdaoCaseManager.Shared.Entites;
+
+public class AuditRoot
+{
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime CreateDate { get; set; }
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime UpdateDate { get; set; }
+}
+
