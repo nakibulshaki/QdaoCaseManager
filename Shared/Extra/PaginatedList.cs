3b8bd56c173f79e3bb84099a1b32242099588c15
Nakibul Shaki <nakibulshaki@hotmail.com>
2024-01-22 01:00:30 -0500

    Add generic for pagination

---
+++ Shared/Extra/PaginatedList.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace QdaoCaseManager.Extra;
+[Serializable]
+public class PaginatedList<T> 
+{
+    public List<T> Items { get; set; }
+    public int PageIndex { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+    public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+    public bool HasPreviousPage => PageIndex > 1;
+    public bool HasNextPage => PageIndex < TotalPages;
+    public PaginatedList(List<T> items, int pageIndex, int pageSize, int totalCount)
+    {
+        Items = items;
+        PageIndex = pageIndex;
+        PageSize = pageSize;
+        TotalCount = totalCount;
+    }
+
+
+}
