f887a2bb994271e13b19ce5379f6b66fe50c6c49
Nakibul Shaki <nakibulshaki@hotmail.com>
2024-01-22 01:00:49 -0500

    fix typo and add migration

---
+++ QdaoCaseManager/QdaoCaseManager/Data/Migrations/20240122025859_Fix-Typo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QdaoCaseManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixTypo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Cases",
+                newName: "Tittle");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Tittle",
+                table: "Cases",
+                newName: "Title");
+        }
+    }
+}
