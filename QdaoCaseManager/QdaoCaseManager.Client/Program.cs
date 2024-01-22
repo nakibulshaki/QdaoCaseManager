f7f84f772b5be8938d11857b4f381c8e68f03676
Nakibul Shaki <nakibulshaki@hotmail.com>
2024-01-21 19:00:13 -0500

    Resolve HttpClient In Client Project

--- QdaoCaseManager/QdaoCaseManager.Client/Program.cs
+++ QdaoCaseManager/QdaoCaseManager.Client/Program.cs
@@ -7,5 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
-
+builder.Services.AddScoped(http => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
+});
 await builder.Build().RunAsync();
